4954afc34fe23bd996ca612efcf1b4dd1843e09d
kasramvd <kasraavand@gmail.com>
2017-02-04 09:53:20 +0330

    add novel class to find top new products

--- General/Novel.py
+++ General/Novel.py
@@ -1,10 +1,31 @@
+"""Novel products for users."""
+
 from UPeT.importer import pre_analyzed_importer
+from datetime import datetime, timedelta
+from .Rexy import config
+from operator import itemgetter
 
 
 class Novel:
-    def __init__(self, *args, **kwargs):
-        self.top_n = kwargs['top_n']
-        self.pre_analyzed_importer = pre_analyzed_importer
+    """Top new products."""
 
-    def select(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        self.top_n = kwargs['novel_number']
+        self.days = kwargs['novel_days']
+        pre_analyzed_db_name = kwargs['pre_analyzed_db_name']
+        pre_importer = pre_analyzed_importer.PreImporter(db_name=pre_analyzed_db_name)
+        self.all_products = pre_importer.import_products()
+
+    def find_novels(self):
+        products = [(p['id'],
+                     p['metadata']['rate'],
+                     p['metadata']['download_numbe'])
+                    for p in self.all_products if self.validate_date(p['metadata']['submit_date'])]
+
+        products = sorted(products, key=itemgetter(1, 2))
+        return products[:self.top_n]
+
+    def validate_date(self, date):
+        date_format = config.date_format
+        days = timedelta(days=self.days)
+        return datetime.now() - datetime.strptime(date, date_format) < days
