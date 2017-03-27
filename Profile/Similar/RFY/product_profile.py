853bf999e1b85b4cb6fbe0d17be0a37946b4ddec
kasramvd <kasraavand@gmail.com>
2017-03-27 14:19:37 +0430

    update the module and bug fixes

--- Profile/Similar/RFY/product_profile.py
+++ Profile/Similar/RFY/product_profile.py
@@ -1,4 +1,3 @@
-from UPeT.importer import pre_analyzed_importer
 from Rexy.Core.general import cal_sim_product
 from itertools import product as itproduct, chain
 from operator import itemgetter
@@ -6,49 +5,46 @@ from operator import itemgetter
 
 class Recommendation:
     def __init__(self, *args, **kwargs):
-        db_name = kwargs['db_name']
-        self.importer = pre_analyzed_importer.PreImporter(db_name=db_name)
-        self.product = kwargs['product']
-        self.user = kwargs['user']
         self.top_tag_number = kwargs['top_tag_number']
         self.top_related_products = kwargs['top_related_products']
 
-    def find_common_tags(self):
-        product_tags = self.product['tags']
-        user_tags = self.user['tags']
+    def find_common_tags(self, product, user):
+        product_tags = product['tags']
+        user_tags = user['tags']
 
         for t, aff in product_tags.items():
             if t in user_tags:
                 yield t, aff, user_tags[t]
 
-    def recom_from_tags(self):
+    def recom_from_tags(self, product, user):
 
         # Sort common tags based on user-tag affinities, since it has more priority.
         try:
-            common_tags = next(zip(*sorted(self.find_common_tags(), key=itemgetter(2))))
+            common_tags = next(zip(*sorted(self.find_common_tags(product, user), key=itemgetter(2))))
         except StopIteration:
             # There's no common tags
-            return self.no_common_tag()
+            return self.no_common_tag(product, user)
         else:
             if common_tags:
                 top_n_tags = common_tags[-self.top_tag_number:]
                 return self.top_n_related_products(top_n_tags)
             else:
-                return self.no_common_tag()
+                return self.no_common_tag(product, user)
 
-    def no_common_tag(self):
+    def no_common_tag(self, product, user):
         # Find top products related to product tags and user tags.
-        tp_product_tag = self.top_n_related_products(self.product['tags'])
-        tp_user_tag = self.top_n_related_products(self.user['tags'])
+        tp_product_tag = self.top_n_related_products(product['tags'])
+        tp_user_tag = self.top_n_related_products(user['tags'])
 
         # Find similar products.
-        sim_products = chain.from_iterables(self.find_similar_products(tp_user_tag, tp_product_tag))
+        sim_products = set(chain.from_iterables(self.find_similar_products(tp_user_tag, tp_product_tag)))
         return sim_products
 
     def find_similar_products(self, products1, products2):
+        # Calculate the similarity of the products that are related to user tags and
+        # The products that are related to the main product itself.
         d = {}
         for p1, p2 in itproduct(products1, products2):
-            print(d)
             d[cal_sim_product(p1['tags'], p2['tags'], return_diff=False)] = [p1, p2]
         for sim, (p1, p2) in sorted(d.items(), itemgetter(0))[-self.top_related_products:]:
             yield p1, p2
