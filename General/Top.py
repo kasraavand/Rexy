2bbe4529ceb192c27d8bbf3827c63aa9c51f59b7
kasramvd <kasraavand@gmail.com>
2017-03-27 14:19:16 +0430

    update the module and bug fixes

--- General/Top.py
+++ General/Top.py
@@ -8,10 +8,8 @@ class Top:
 
     def __init__(self, *args, **kwargs):
         self.top_n = kwargs['top_number']
-        pre_analyzed_db_name = kwargs['pre_analyzed_db_name']
-        pre_importer = pre_analyzed_importer.PreImporter(db_name=pre_analyzed_db_name)
-        self.all_products = pre_importer.import_products()
+        self.all_products = kwargs['all_products']
 
-    def find_top(self):
+    def find_tops(self):
         products = [(p['id'], p['rating'], p['download_number']) for p in self.all_products]
         return sorted(products, key=itemgetter(1, 2))[:self.top_n]
